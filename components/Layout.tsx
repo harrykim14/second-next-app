32eb27b11c8ba195782cf5d375713be8c14f8953
harrykim14 <harrykim14@korea.ac.kr>
2021-05-21 18:12:22 +0900

    Page update 05.21

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Link from "next/link";
+import Image from 'next/image'
 import { Grid } from "@material-ui/core";
 
 interface TITLE {
@@ -18,11 +19,21 @@ const Layout: React.FC<TITLE> = ({ children, title }) => {
           className="w-screen h-24 topNav"
         >
           <Grid container>
+            <Grid item xs={1}>
+              <div className="flex justify-center items-center m-2 cursor-pointer">
+                <Link href="/">
+              <Image src="/images/icon.jpg" alt="Icon" width={320} height={180} className="rounded"/>
+              </Link>
+              </div>
+            </Grid>
+            <Grid item xs={11}>
             <Grid container item>
-              <Grid item xs={3}>
-                <div className="bg-white h-10 m-1 p-2 flex justify-center items-center rounded">
-                  <Link href="/color">color</Link>
+              <Grid item xs={3} className="cursor-pointer">
+              <Link href="/basic">
+                <div className="bg-white h-10 m-1 p-2 flex justify-center items-center rounded font-bold text-xl transition-opacity duration-500 ease-in hover:bg-green-100">
+                  서러브레드 기본 정보
                 </div>
+                </Link>
               </Grid>
               <Grid item xs={3}>
                 <div className="bg-white h-10 m-1 p-2 flex justify-center items-center rounded">
@@ -56,6 +67,7 @@ const Layout: React.FC<TITLE> = ({ children, title }) => {
                   Navigation Bar 2
                 </div>
               </Grid>
+            </Grid>
             </Grid>
           </Grid>
         </nav>
---
+++ pages/basic.tsx
@@ -0,0 +1,135 @@
+import Layout from "../components/Layout";
+import Image from "next/image";
+import { Grid } from "@material-ui/core";
+import { VscTriangleDown } from "react-icons/vsc";
+
+const color = () => {
+  return (
+    <Layout title="Color">
+      <div>
+        <Grid container>
+          <Grid item container xs={4} className="flex">
+            <Grid item xs={12} className="flex justify-center top-0">
+              <button className="text-3xl font-bold px-4 m-2 rounded h-16 cursor-default border-4 border-yellow-900">
+                서러브레드의 일생
+              </button>
+            </Grid>
+            <Grid
+              item
+              xs={12}
+              className="p-4 h-screen text-center justify-center items-center"
+            >
+              교배(種付け)
+              <VscTriangleDown className="m-auto" />
+              태어남
+              <VscTriangleDown className="m-auto" />
+              젖떼기
+              <VscTriangleDown className="m-auto" />
+              기초체력 단련
+              <VscTriangleDown className="m-auto" />
+              재갈, 뱃대끈, 장제, 승마 길들이기 등의 조교
+              <VscTriangleDown className="m-auto" />
+              트레이닝 센터로 들어감
+              <VscTriangleDown className="m-auto" />
+              데뷔
+              <VscTriangleDown className="m-auto" />
+              레이스
+              <VscTriangleDown className="m-auto" />
+              은퇴
+            </Grid>
+          </Grid>
+          <Grid item container xs={8}>
+            <Grid
+              item
+              xs={6}
+              className="flex flex-col justify-center items-center"
+            >
+              <button className="bg-yellow-900 text-white text-4xl font-bold px-4 m-2 rounded h-16 cursor-default">
+                갈색(鹿毛)
+              </button>
+              <div>
+                <Image
+                  src="/images/basic/gran_alegria_640.jpg"
+                  width={640}
+                  height={480}
+                  alt="dear brown horse"
+                />
+              </div>
+            </Grid>
+            <Grid item xs={6} className="flex flex-col justify-center items-center">
+              <button className="bg-gray-900 text-white text-4xl font-bold px-4 m-2 rounded h-16 cursor-default">
+                흑록색(黒鹿毛)
+              </button>
+              <Image
+                src="/images/basic/stella_velocie_640.jpg"
+                width={640}
+                height={480}
+                alt="blue black horse"
+              />
+            </Grid>
+            <Grid
+              item
+              xs={6}
+              className="flex flex-col justify-center items-center"
+            >
+              <button className="bg-green-900 text-white text-4xl font-bold px-4 m-2 rounded h-16 cursor-default">
+                청록색(青鹿毛)
+              </button>
+              <Image
+                src="/images/basic/cool_cat_640.jpg"
+                width={640}
+                height={480}
+                alt="blue black horse"
+              />
+            </Grid>
+            <Grid item xs={6} className="flex flex-col justify-center items-center">
+              <button className="bg-blue-900 text-white text-4xl font-bold px-4 m-2 rounded h-16 cursor-default">
+                청흑색(青毛)
+              </button> 
+              <Image
+                src="/images/basic/cesario_640.jpg"
+                width={640}
+                height={480}
+                alt="blue black horse"
+              />
+            </Grid>
+            <Grid item xs={6} className="flex flex-col justify-center items-center">
+              <button className="bg-gray-400 text-white text-4xl font-bold px-4 m-2 rounded h-16 cursor-default">
+                회백색(芦毛)
+              </button>
+              <Image
+                src="/images/basic/smile_kana_640.jpg"
+                width={640}
+                height={480}
+                alt="gray horse"
+              />
+            </Grid>
+            <Grid item xs={6} className="flex flex-col justify-center items-center">
+              <button className="bg-yellow-600 text-white text-4xl font-bold px-4 m-2 rounded h-16 cursor-default">
+                밤색(栗毛)
+              </button>
+              <Image
+                src="/images/basic/ho_o_amazon_640.jpg"
+                width={640}
+                height={480}
+                alt="chest-nut horse"
+              />
+            </Grid>
+            <Grid item xs={6} className="flex flex-col justify-center items-center">
+              <button className="bg-yellow-900 text-white text-4xl font-bold px-4 m-2 rounded h-16 cursor-default">
+                진한밤색(栃栗毛)
+              </button>
+            </Grid>
+            <Grid item xs={6} className="flex flex-col justify-center items-center">
+              <button className="border-4 border-black-900 text-black text-4xl font-bold px-4 m-2 rounded h-16 cursor-default">
+                흰색(白毛)
+              </button>
+            </Grid>
+          </Grid>
+        </Grid>
+      </div>
+    </Layout>
+  );
+};
+
+export default color;
